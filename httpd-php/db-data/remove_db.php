ccd6d678baf64ef040e0bb11f4ae37179b892b02
Linux User <you@domain.com>
2022-01-12 21:34:48 +0000

    Commit Projet

---
+++ httpd-php/db-data/remove_db.php
@@ -0,0 +1,24 @@
+<?php
+   $dbhost = '192.168.1.29';
+   $dbuser = 'jpereira';
+   $dbpass = 'jpereira';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   echo "Connected successfully\n";
+   
+   $sql = 'DROP Database test_db';
+   $retval = mysql_query( $sql, $conn );
+   
+   echo  "Database test removed successfully";
+
+   if(! $retval ) {
+      die('Could not create database: ' . mysql_error());
+   
+
+   }
+   mysql_close($conn);
+?>
